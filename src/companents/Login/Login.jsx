e6f117b0ed9d179d4b74c125a301e528552635f5
Joen <joen01@mail.ru>
2025-02-22 16:45:18 +0300

    75 formik

--- src/companents/Login/Login.jsx
+++ src/companents/Login/Login.jsx
@@ -1,10 +1,41 @@
-import React from "react";
-import f from  "./Login.module.css"
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
 
- const Login = () => {
-    return ( <div className={f.item}>
-            You are not logged in
-        </div>
-    )
+const LoginForm = () => {
+    return (
+        <Formik
+            initialValues={{ Email : '', password: '', rememberMe: false }}
+            onSubmit={(values) => {
+                // Обработка отправки формы
+                console.log(values);
+            }}
+        >
+            {() => (
+                <Form>
+                    <div>
+                        <Field name="Email" placeholder="Email" />
+                    </div>
+                    <div>
+                        <Field name="password" type="password" placeholder="Password" />
+                    </div>
+                    <div>
+                        <Field name="rememberMe" type="checkbox" />
+                        <label htmlFor="rememberMe"> remember me</label>
+                    </div>
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
+                </Form>
+            )}
+        </Formik>
+    );
+};
+
+
+const Login = () => {
+    return <div>
+        <h1>Login</h1>
+        <LoginForm/>
+    </div>
 }
 export default Login
